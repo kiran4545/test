98d61d75913e36f91b77fdb9d96a5ccedcf5f93f
kiran4545 <phskramaraju@gmail.com>
2019-07-09 15:56:29 +0530

    Hunger

--- hunger.py
+++ hunger.py
@@ -9,3 +9,13 @@ print("Kiran is very hungry")
 print("I am very hungry")
 print("Mani is very hungry")
 
+<<<<<<< HEAD
+=======
+
+
+i am raju
+i am kiran
+i am human
+i am human being
+
+>>>>>>> 7836365... Added human being
